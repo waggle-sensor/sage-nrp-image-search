30f56ba74c2c6cd8b9e6e43fe968ac1adb65b69b
FranciscoLozCoding <francisco.lozano33@outlook.com>
2026-03-27 17:04:35 -0500

    switch back to vectorizer_config until the whole version change is done

--- benchmarking/benchmarks/Commonobjectsbench/data_loader.py
+++ benchmarking/benchmarks/Commonobjectsbench/data_loader.py
@@ -170,7 +170,7 @@ class CommonObjectsBenchDataLoader(DataLoader):
                 Property(name="tags", data_type=DataType.TEXT),
                 Property(name="confidence", data_type=DataType.TEXT),
             ],
-            "vector_config": [
+            "vectorizer_config": [
                 Configure.NamedVectors.none(
                     name=TARGET_VECTOR,
                     vector_index_config=Configure.VectorIndex.hnsw(
